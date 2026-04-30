23bbc51aa7b5b771a7f190a8f444d902f3ab5335
Clarissa11092001 <yoselin.gonzalez@catolica.edu.sv>
2026-04-29 21:52:51 -0600

    Creacion de vista login

--- index.php
+++ index.php
@@ -1,11 +1,59 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login Pro - Animated</title>
+    <link rel="stylesheet" href="assets/Css/index.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
 </head>
 <body>
-    
+
+    <div class="background-animate">
+        <span style="--i:11;"></span>
+        <span style="--i:12;"></span>
+        <span style="--i:24;"></span>
+        <span style="--i:10;"></span>
+        <span style="--i:14;"></span>
+        <span style="--i:23;"></span>
+        <span style="--i:18;"></span>
+        <span style="--i:16;"></span>
+        <span style="--i:19;"></span>
+        <span style="--i:20;"></span>
+    </div>
+
+    <div class="main-container">
+        
+        <div class="header-logo">
+            <img src="assets/Img/MUNIFY.jpeg" alt="Munify Logo">
+            
+        </div>
+
+    <div class="login-box">
+        <form action="#" method="POST">
+            <h2>Iniciar Sesión</h2>
+            
+            <div class="input-group">
+                <input type="text" required>
+                <label>Usuario</label>
+                <i></i>
+            </div>
+            
+            <div class="input-group">
+                <input type="password" required>
+                <label>Contraseña</label>
+                <i></i>
+            </div>
+
+            <div class="links">
+                <a href="#">Olvidé mi clave</a>
+                <a href="#">Registrarse</a>
+            </div>
+
+            <input type="submit" value="Ingresar" class="btn-submit">
+        </form>
+    </div>
+    </div>
+
 </body>
 </html>
