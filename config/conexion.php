c2f17fce2081482d2f723b30f8af4fda26e1caf5
Lisseth-Montoya <veronica.montoya@catolica.edu.sv>
2026-05-11 20:33:10 -0600

    Ajustes finales de conexion

--- config/conexion.php
+++ config/conexion.php
@@ -1,14 +1,21 @@
 <?php
-$host = "127.0.0.1";
-$port = "3308";
-$user = "root";
-$password = "";
-$dbname = "Munify";
+class Conexion {
+    private $host = 'localhost';
+    private $db_name = 'bdd_munify';
+    private $username = 'root';
+    private $password = '';
+    public $conn;
 
-$conn = new mysqli($host, $user, $password, $dbname, $port);
-
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+    public function conectar() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->exec("set names utf8");
+        } catch(PDOException $exception) {
+            echo "Error de conexión: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
 }
-$conn->set_charset("utf8");
 ?>
